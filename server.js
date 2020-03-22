75bf05239a48915764f35b32b9af18af76932668
caimichael88 <caimichael88@gmail.com>
2020-03-22 11:02:26 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/', express.static('public'));
+
+/*app.get('/', (req, res) => {
+    //res.send('<h1>Hello htere</h1>');
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})*/
+
+const PORT = process.env.ORT || 8080;
+
+app.get('*', (req, res) => {
+    res.status(400).json({ message: 'page doesnot exist' });
+})
+
+app.listen(PORT, ()=>{
+    console.log('listing on port 8080');
+})
